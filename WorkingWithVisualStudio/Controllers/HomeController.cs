50e63a2f8a1105a8a32c16f073a4da4d58295788
Smith <JCSmith1@co.pg.md.us>
2018-08-25 14:09:09 -0400

    Add project files.

---
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkingWithVisualStudio.Models;
+
+
+namespace WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+            => View(SimpleRepository.SharedRepository.Products);
+    }
+}
